5a71703c78c92f93bac301341d14f42bf5d579dc
David Calle <davidcalleg@gmail.com>
2022-05-08 18:20:31 -0500

    styles fix

--- resources/views/pokenea.php
+++ resources/views/pokenea.php
@@ -3,9 +3,9 @@
 <body>
     <div style="width: 50%;margin: 0 auto;">
         <img style="height: 400px; display: block; margin: 0 auto;" src="<?php echo $viewData['pokenea']['image']; ?>" />
-        <cite style="text-align: center;"><?php echo $viewData['pokenea']['phrase'] ?></q>
-            <p style="text-align: center;">Traido a usted desde: <?php echo $viewData['server_ip'] ?>
-        </cite>
+
+        <q style="text-align: center; display: block;"><?php echo $viewData['pokenea']['phrase'] ?></q>
+        <p style="text-align: center;">Traido a usted desde: <?php echo $viewData['server_ip'] ?>
     </div>
 </body>
 
