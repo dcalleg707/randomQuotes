c7f541a5b68aa28189f6ba0e40df2f3b2fcb0807
David Calle <davidcalleg@gmail.com>
2022-05-08 17:57:59 -0500

    new styles por random/image

--- resources/views/pokenea.php
+++ resources/views/pokenea.php
@@ -1,9 +1,12 @@
 <html>
 
 <body>
-    <img src="<?php echo $viewData['pokenea']['image']; ?>" />
-    <p><?php echo $viewData['pokenea']['phrase'] ?></p>
-    <p><?php echo $viewData['server_ip'] ?></p>
+    <div style="width: 50%;margin: 0 auto;">
+        <img style="height: 400px; display: block; margin: 0 auto;" src="<?php echo $viewData['pokenea']['image']; ?>" />
+        <cite style="text-align: center;"><?php echo $viewData['pokenea']['phrase'] ?></q>
+            <p style="text-align: center;">Traido a usted desde: <?php echo $viewData['server_ip'] ?>
+        </cite>
+    </div>
 </body>
 
 </html>
